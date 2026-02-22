042ba0e68fda4f631deb681d65b379ad0a4a838c
Baymax626 <13665376234@163.com>
2026-02-22 21:20:46 +0800

    数据集加载

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+
 import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
 from torchvision.transforms import transforms
 
 batch_size = 64
@@ -6,7 +9,35 @@ lr = 0.001
 epochs = 10
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+print(f"当前使用{device}")
+
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,)) ])
+    transforms.Normalize((0.1307,), (0.3081,))
+])
 
+train_dataset = datasets.MNIST(
+    root="./data",
+    train=True,
+    download=True,
+    transform=transform
+)
+
+test_dataset = datasets.MNIST(
+    root="./data",
+    train=False,
+    download=True,
+    transform=transform
+)
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=batch_size,
+    shuffle=True
+)
+
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=batch_size,
+    shuffle= False
+)
