ac70401468045ff742cda2f1356af6e558ee8fb1
Baymax626 <13665376234@163.com>
2026-02-22 21:15:54 +0800

    设置超参数

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import torch
 from torchvision.transforms import transforms
+
+batch_size = 64
+lr = 0.001
+epochs = 10
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 transform = transforms.Compose([
     transforms.ToTensor(),
